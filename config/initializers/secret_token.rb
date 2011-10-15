c2f5f150477032ed3a54f128ed7a7b4a63e8f3b8
Calvin Park <nofxincubus@gmail.com>
2011-10-15 18:12:58 -0400

    All Files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mlogin::Application.config.secret_token = 'd00af51e40d2d9502e15e209ed78b69cbcf2e91dfeb744818c33e6d42db382847ce0bffdc73d152897cb00049c76afbf4f06317ceefe68c49cdb98f5520f373a'
